cc5d58fe740260370d3813eb747750399090f254
dulguxun <duk.dulguun46@gmail.com>
2022-11-15 14:37:52 +0800

    medkuu boloosoi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const body = document.body;
+
+
+const hamburgerToggler = document.querySelector(".toggler");
+
+
+hamburgerToggler.addEventListener("change", (e) => {
+  e.target.checked
+    ? (body.style.overflowY = "hidden")
+    : (body.style.overflowY = "scroll");
+});
